5e19b71f4bdd3ae94637f0bfe5bf33eb44df111a
Zhenhao Lu <zhenhao@usc.edu>
2024-03-10 21:02:17 -0700

    JWT Update

--- src/main/java/com/example/demo/controller/ControllerHipoLab.java
+++ src/main/java/com/example/demo/controller/ControllerHipoLab.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.University;
-//import com.example.demo.service.impl.HipoLabService;
 import com.example.demo.service.inter.IHipoLabService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
